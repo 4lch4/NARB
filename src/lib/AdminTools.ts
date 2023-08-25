eee8ff788e8d8203cfbaa653d24f7af12046f2c1
Devin W. Leaman <git@4lch4.email>
2023-08-25 03:44:24 -0500

    feat: add optional mention parameter to sendImportantMessage

--- src/lib/AdminTools.ts
+++ src/lib/AdminTools.ts
@@ -8,11 +8,13 @@ export class BotTools {
 
   public constructor(private bot: Client) {}
 
-  public async sendImportantMessage(message: string) {
-    return this.sendMessage(
-      HOMEPAGE_CHANNEL_ID,
-      `Hey <@219270060936527873>, got an important message for ya:\n\n${message}`
-    )
+  public async sendImportantMessage(message: string, mention: boolean = false) {
+    return mention
+      ? this.sendMessage(
+          HOMEPAGE_CHANNEL_ID,
+          `Hey <@219270060936527873>, got an important message for ya:\n\n${message}`
+        )
+      : this.sendMessage(HOMEPAGE_CHANNEL_ID, `Got an important message for ya:\n\n${message}`)
   }
 
   public async sendMessage(channelId: string, message: string) {
